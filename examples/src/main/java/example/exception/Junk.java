c21cdbd1b7e5aa8f1871684aeb13c24d27316292
puppylpg <shininglhb@163.com>
2022-06-15 00:09:52 +0800

    Add Exception explanation

--- examples/src/main/java/example/exception/Junk.java
+++ examples/src/main/java/example/exception/Junk.java
@@ -1,6 +1,28 @@
 package example.exception;
 
 /**
+ * 每一个e由三部分组成：
+ * - message；
+ * - stacktrace：jdk自然知道当前调用栈是哪儿，所以会把从main函数一直到new exception的地方的调用栈都放到stacktrace里。
+ *  既然是栈，所以先记录最里层的stack，最后记main所在的stack；
+ * - e：如果new exception的时候不传e，则e=this。也就是说默认exception的e是自己，是个自引用。如果传了，就指向传进来的e。
+ *
+ * 一般我们在new外层的exception（比如文中的HighLevelException）的时候，会把内层产生的e传入这个exception。所以e一般是这样的：
+ * 1. 最外层的e（eg：HighLevelException），它的栈帧就是从main到new它的函数，倒序存放；它的e就是内层的exception；
+ *  1.1 内层的e（eg：MidLevelException），它的栈帧就是从main到new它的函数，所以会比外层的e多几层；它的e就是最里层的exception；
+ *   1.2 最内层的e（eg：LowLevelException），它的栈帧就是从main到new它的函数，所以它的栈帧最多；它的e就是它自己，没啥意义，感觉jdk设成null或者EMPTY其实就行了……
+ *
+ * 所以print stacktrace的时候，print的是最外层的e，而它会先打它的message，再打它的stacktrace，最后打它的e；
+ * 于是递归地把内层的e也按照message、stack、内层e的顺序打出来；
+ * 直到达到最里面的e。
+ * 所以打印异常的姿势是：
+ * 1，最先打印的是最外层的异常，最后打印的是最里层的；
+ * 2. 每个e的stacktrace都是从main到这个异常new的地方；
+ * 当然打印最里面的e的时候，可能由于stacktrace太长且和外面重复，会省略最外面的stack，因为之前的e已经打过这些stack了。
+ * 所以我们看log里的异常的时候，直接拉到最下面看e就行了，它就是罪魁祸首。
+ *
+ * 最离谱的是：exception getCause的时候，return (cause==this ? null : cause)……如果e=this，返回null……说明没e了……那为啥不一开始就设为null或者EMPTY……
+ *
  * @author liuhaibo on 2019/07/29
  */
 public class Junk {
