fe1b37b073940ea55b9790a636fe902c5ca4e3d7
puppylpg <shininglhb@163.com>
2023-08-11 22:43:29 +0800

    inner class & enum

--- examples/src/main/java/example/enums/Season.java
+++ examples/src/main/java/example/enums/Season.java
@@ -1,7 +1,12 @@
 package example.enums;
 
+import lombok.NonNull;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author liuhaibo on 2018/01/11
@@ -40,6 +45,8 @@ public enum Season {
         this.text = text;
     }
 
+    private static final Map<String, Season> REVERSE_LOOKUP = Arrays.stream(Season.values()).collect(Collectors.toMap(e -> e.getText().toLowerCase(), Function.identity()));
+
     private static final Map<Integer, Season> indexToSeasons = new HashMap<>();
 
     static {
@@ -70,6 +77,18 @@ public enum Season {
         return Season.valueOf(text);
     }
 
+    public static Season parseByText2(@NonNull String value) {
+        return REVERSE_LOOKUP.getOrDefault(value.toLowerCase(), UNKNOWN);
+    }
+
+    public static Season parseByText3(@NonNull String value) {
+        try {
+            return Season.valueOf(value);
+        } catch (Exception e) {
+            return UNKNOWN;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Print all the seasons:");
         indexToSeasons.forEach((x, y) -> System.out.println(x + y.getText()));
@@ -77,5 +96,17 @@ public enum Season {
         System.out.println("Print season 2:");
         System.out.println(parseByIndex(2));
         System.out.println(parseByText("WINTER"));
+        System.out.println(parseByText2("WINTER"));
+        System.out.println(parseByText3("WINTER"));
+        try {
+            // No enum constant example.enums.Season.nono
+            System.out.println(parseByText("nono"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // UNKNOWN
+        System.out.println(parseByText2("nono"));
+        // UNKNOWN
+        System.out.println(parseByText3("nono"));
     }
 }
--- examples/src/main/java/example/innerclass/Outer.java
+++ examples/src/main/java/example/innerclass/Outer.java
@@ -5,25 +5,30 @@ package example.innerclass;
  */
 public class Outer {
 
-    private int a;
+    private int out;
 
     private int access1() {
-        return a;
+        return out;
     }
 
     private int access2() {
-        return Outer.this.a;
+        // 构造内部类的时候，外部类就被传入内部类了。javac干的
+        return new Inner().in;
     }
 
     private class Inner {
-        private int b;
+        private int in;
+        // javac还会多生成这样一个属性，就可以通过它访问创建内部类的外部类的数据了
+//        Outer this$0;
 
         private int access1() {
-            return a;
+            // 等同于Outer.this.out;
+            return out;
         }
 
         private int access2() {
-            return Outer.this.a;
+            // 其实就是调用this$0.out
+            return Outer.this.out;
         }
     }
 }
