fe83647819fdfb966cd82922885a33f546b3f1a3
puppylpg <shininglhb@163.com>
2023-09-24 22:30:15 +0800

    jdk21

---
+++ 8-plus/normal/src/main/java/xyz/puppylpg/vthread/VThreadInfo.java
@@ -0,0 +1,18 @@
+package xyz.puppylpg.vthread;
+
+import java.time.Duration;
+
+/**
+ * @author puppylpg on 2023/09/24
+ */
+public class VThreadInfo {
+
+    public static void main(String... args) throws InterruptedException {
+        Thread v = Thread.ofVirtual().start(() -> {
+            // VirtualThread[#21]/runnable@ForkJoinPool-1-worker-1
+            System.out.println(Thread.currentThread());
+        });
+
+        v.join();
+    }
+}
--- examples/src/main/java/example/singleton/instances/StaticInnerClass.java
+++ examples/src/main/java/example/singleton/instances/StaticInnerClass.java
@@ -1,9 +1,17 @@
 package example.singleton.instances;
 
+import java.lang.reflect.Array;
+import java.util.List;
+
 /**
  * @author liuhaibo on 2017/12/26
  */
 public class StaticInnerClass {
+
+    public static <T> T[] convert(List<T> list, Class<T> type) {
+        T[] array = (T[]) Array.newInstance(type, list.size());
+        return array;
+    }
 
     private StaticInnerClass() {}
 
