89779b2b23d2bf90d5f8765fbc5ab4c8a60ae19c
puppylpg <shininglhb@163.com>
2024-10-27 21:03:49 +0800

    stacktrace optimization

---
+++ examples/src/main/java/example/exception/optimize/DefaultExceptionOptimize.java
@@ -0,0 +1,38 @@
+package example.exception.optimize;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+/**
+ * @author puppylpg on 2024/10/27
+ */
+public class DefaultExceptionOptimize {
+
+    public static void main(String[] args) {
+        String msg = null;
+
+        String rawTrace = null, optimizedTrace = null;
+        int changedTimes = 0;
+
+        for (int i = 0; i < 500000; i++) {
+            try {
+                msg.toString();
+            } catch (Exception e) {
+                String trace = ExceptionUtils.getStackTrace(e);
+                System.err.println(trace);
+                if (StringUtils.isEmpty(rawTrace)) {
+                    rawTrace = trace;
+                }
+
+                if (StringUtils.isEmpty(optimizedTrace) && !rawTrace.equals(trace)) {
+                    optimizedTrace = trace;
+                    changedTimes = i;
+                }
+            }
+        }
+
+        System.out.println("rawTrace = " + rawTrace);
+        System.out.println("optimizedTrace = " + optimizedTrace);
+        System.out.println("changedTimes = " + changedTimes);
+    }
+}
